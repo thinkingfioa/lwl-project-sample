3f1719c2bae2ac2a33b61cbd7995b5789f316de6
thinking_fioa <thinking_fioa@163.com>
2017-12-27 20:53:27 +0800

    accomplish Controller

---
+++ project-sample-web/src/main/java/com/thinking/project/sample/web/controller/DemoController.java
@@ -0,0 +1,11 @@
+package com.thinking.project.sample.web.controller;
+
+/**
+ * @author thinking_fioa
+ * @createTime 2017/12/27
+ * @description Controller的Demo
+ */
+
+
+public class DemoController {
+}
